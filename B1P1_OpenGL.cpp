1e283587d35e501df35fb890dd19f963b566f80f
Darlin Joel Anacicha <105738470+DJoel20@users.noreply.github.com>
2025-02-02 18:23:29 -0500

    Add files via upload

--- B1P1_OpenGL.cpp
+++ B1P1_OpenGL.cpp
@@ -34,9 +34,6 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-glm::vec3 fixedLightPos(1.0f, 1.0f, 1.0f); // Luz con posición fija
-
 
 int main()
 {
@@ -75,8 +72,7 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
-    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+        
     stbi_set_flip_vertically_on_load(true);
 
     // configure global opengl state
@@ -87,9 +83,9 @@ int main()
     // -------------------------
     Shader ourShader("shaders/shader_exercise16_mloading.vs", "shaders/shader_exercise16_mloading.fs");
 
+   
     // load models
     // -----------
-    //Model ourModel(FileSystem::getPath("resources/objects/backpack/backpack.obj"));
     Model landscapeModel("model/map-landscape/scene.obj");
     Model concertModel("model/map-concert/scene.obj");
     Model tvModel("model/television/scene.obj");
@@ -98,8 +94,7 @@ int main()
     Model djModel("model/dj/scene.obj");
     Model dronModel("model/dron/scene.obj");
 
-    //Model ourModel("model/backpack/backpack.obj");
-
+    //Struct de luces
     struct Light {
         glm::vec3 position;
         glm::vec3 direction;
@@ -123,18 +118,17 @@ int main()
     ourShader.setInt("material.diffuse", 0);
     ourShader.setInt("material.specular", 1);
 
+    //Posicion de la luces
     glm::vec3 ligthPos[5];
     ligthPos[0] = glm::vec3(1.2f, 1.0f, 2.0f);
     ligthPos[1] = glm::vec3(3.0f, 2.0f, -2.0f);
     ligthPos[2] = glm::vec3(2.0f, 2.5f, 2.0f);
     ligthPos[3] = glm::vec3(0.0f, 3.0f, -3.0f);
-    ligthPos[4] = glm::vec3(-3.0f, 1.5f, 3.0f);
+    ligthPos[4] = glm::vec3(6.0f, 1.5f, 3.0f);
 
 
-    // draw in wireframe
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    camera.MovementSpeed = 5.0; //Optional. Modify the speed of the camera
+    
+    camera.MovementSpeed = 5.0; //Velocidad de la camara
 
     // render loop
     // -----------
@@ -157,14 +151,14 @@ int main()
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        // don't forget to enable shader before setting uniforms
+        
         ourShader.use();
 
-
+        
         // Inicializar las luces
         for (int i = 0; i < 5; i++) {
-            lights[i].position = ligthPos[i];  // Ejemplo de posiciones
-            lights[i].direction = glm::vec3(0.0f, -1.0f, 0.0f);       // Ejemplo de direcciones
+            lights[i].position = ligthPos[i];  
+            lights[i].direction = glm::vec3(0.0f, -1.0f, 0.0f);       
             lights[i].ambient = glm::vec3(0.1f, 0.1f, 0.1f);
             lights[i].diffuse = glm::vec3(0.8f, 0.8f, 0.8f);
             lights[i].specular = glm::vec3(1.0f, 1.0f, 1.0f);
@@ -178,7 +172,7 @@ int main()
         // Pasar las luces al shader
         for (int i = 0; i < 5; i++) {
             float angle = currentFrame * lightMovementSpeed;
-            float radius = 1.0f;
+            float radius = 5.0f;
             lights[i].position = glm::vec3(radius * cos(angle), 2.0f, radius * sin(angle));
             lights[i].direction = glm::vec3(sin(angle), 0.0f, cos(angle));
             std::string index = std::to_string(i);
@@ -195,25 +189,7 @@ int main()
         }
 
          
-        //Exercise 15 Task 4
-        /*ourShader.setVec3("light.position", camera.Position);
-        ourShader.setVec3("light.direction", camera.Front);
-        ourShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
-        ourShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
-        ourShader.setVec3("viewPos", camera.Position);
-
-        // Luz fija
-        ourShader.setVec3("fixedLight.position", fixedLightPos);
-        ourShader.setVec3("fixedLight.direction", glm::vec3(0.0f, -1.0f, 0.0f)); // Dirección hacia abajo (ajustar según el comportamiento deseado)
-
-        ourShader.setFloat("light.constant", 1.0f);
-        ourShader.setFloat("light.linear", 0.09f);
-        ourShader.setFloat("light.quadratic", 0.032f);
-
-        ourShader.setFloat("fixedLight.constant", 1.0f);
-        ourShader.setFloat("fixedLight.linear", 0.09f);
-        ourShader.setFloat("fixedLight.quadratic", 0.032f);*/
-
+        
         // material properties
         ourShader.setFloat("material.shininess", 32.0f);
 
@@ -229,59 +205,59 @@ int main()
 
         // Renderizar el primer modelo (map-landscape)
         glm::mat4 model1 = glm::mat4(1.0f);
-        model1 = glm::translate(model1, glm::vec3(-3.0f, 0.0f, 0.0f)); // Mover a la izquierda
+        model1 = glm::translate(model1, glm::vec3(-3.0f, 0.0f, 0.0f)); 
         model1 = glm::scale(model1, glm::vec3(0.1f, 0.1f, 0.1f));
         ourShader.setMat4("model", model1);
         landscapeModel.Draw(ourShader);
 
         // Renderizar el segundo modelo (map-concert)
         glm::mat4 model2 = glm::mat4(1.0f);
-        model2 = glm::translate(model2, glm::vec3(3.0f, 0.0f, 0.0f)); // Mover a la derecha
+        model2 = glm::translate(model2, glm::vec3(3.0f, 0.0f, 0.0f)); 
         model2 = glm::scale(model2, glm::vec3(0.1f, 0.1f, 0.1f));
         ourShader.setMat4("model", model2);
         concertModel.Draw(ourShader);
 
         // Renderizar el segundo modelo (television)
         glm::mat4 model3 = glm::mat4(1.0f);
-        model3 = glm::translate(model3, glm::vec3(5.5f, 0.0f, 0.0f)); // Mover a la derecha
-        model3 = glm::scale(model3, glm::vec3(0.02f, 0.02f, 0.02f)); // Reduce el tamaño a la mitad
+        model3 = glm::translate(model3, glm::vec3(5.5f, 0.0f, 0.0f));
+        model3 = glm::scale(model3, glm::vec3(0.02f, 0.02f, 0.02f)); 
         ourShader.setMat4("model", model3);
         tvModel.Draw(ourShader);
 
         // Renderizar la segunda TV (duplicado)
         glm::mat4 model4 = glm::mat4(1.0f);
-        model4 = glm::translate(model4, glm::vec3(1.0f, 0.0f, 0.0f)); // Nueva posición
-        model4 = glm::scale(model4, glm::vec3(0.02f, 0.02f, 0.02f));  // Mismo escalado
+        model4 = glm::translate(model4, glm::vec3(1.0f, 0.0f, 0.0f)); 
+        model4 = glm::scale(model4, glm::vec3(0.02f, 0.02f, 0.02f)); 
         ourShader.setMat4("model", model4);
         tvModel.Draw(ourShader);
 
         // Renderizar la speakers (speakers)
         glm::mat4 model5 = glm::mat4(1.0f);
-        model5 = glm::translate(model5, glm::vec3(5.5f, 0.0f, 2.0f)); // Nueva posición
+        model5 = glm::translate(model5, glm::vec3(5.5f, 0.0f, 2.0f)); 
         model5 = glm::rotate(model5, glm::radians(-45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        model5 = glm::scale(model5, glm::vec3(0.03f, 0.03f, 0.03f));  // Mismo escalado
+        model5 = glm::scale(model5, glm::vec3(0.03f, 0.03f, 0.03f));  
         ourShader.setMat4("model", model5);
         speakersModel.Draw(ourShader);
 
         // Renderizar la speakers (speakers2)
         glm::mat4 model6 = glm::mat4(1.0f);
-        model6 = glm::translate(model6, glm::vec3(1.0f, 0.0f, 2.0f)); // Nueva posición
+        model6 = glm::translate(model6, glm::vec3(1.0f, 0.0f, 2.0f)); 
         model6 = glm::rotate(model6, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        model6 = glm::scale(model6, glm::vec3(0.03f, 0.03f, 0.03f));  // Mismo escalado
+        model6 = glm::scale(model6, glm::vec3(0.03f, 0.03f, 0.03f)); 
         ourShader.setMat4("model", model6);
         speakersModel.Draw(ourShader);
 
         // Renderizar la character (character)
         glm::mat4 model7 = glm::mat4(1.0f);
-        model7 = glm::translate(model7, glm::vec3(3.0f, 0.2f, 0.001f)); // Nueva posición
-        model7 = glm::scale(model7, glm::vec3(0.5f, 0.5f, 0.5f));  // Mismo escalado
+        model7 = glm::translate(model7, glm::vec3(3.0f, 0.2f, 0.001f));
+        model7 = glm::scale(model7, glm::vec3(0.5f, 0.5f, 0.5f));
         ourShader.setMat4("model", model7);
         charModel.Draw(ourShader);
 
         // Renderizar la dj (dj)
         glm::mat4 model8 = glm::mat4(1.0f);
-        model8 = glm::translate(model8, glm::vec3(3.0f, 0.4f, 0.005f)); // Nueva posición
-        model8 = glm::scale(model8, glm::vec3(0.003f, 0.003f, 0.003f));  // Mismo escalado
+        model8 = glm::translate(model8, glm::vec3(3.0f, 0.4f, 0.005f)); 
+        model8 = glm::scale(model8, glm::vec3(0.003f, 0.003f, 0.003f)); 
         ourShader.setMat4("model", model8);
         djModel.Draw(ourShader);
 
@@ -290,50 +266,53 @@ int main()
         float angle = currentFrame * lightMovementSpeed;
 
         glm::mat4 model9 = glm::mat4(1.0f);
-        model9 = glm::translate(model9, glm::vec3(1.2f, 1.0f, 2.0f)); // Nueva posición
-        model9 = glm::scale(model9, glm::vec3(0.05f, 0.05f, 0.05f));  // Mismo escalado
-        model9 = glm::rotate(model9, angle, glm::vec3(sin(angle), -1.0f, cos(angle)));
-        model9 = glm::translate(model9, glm::vec3(radius * cos(angle), 2.0f, radius * sin(angle)));
+        model9 = glm::translate(model9, glm::vec3(1.2f, 1.0f, 2.0f)); 
+        model9 = glm::rotate(model9, angle, glm::vec3(-sin(angle), -1.0f, -cos(angle))); 
+        model9 = glm::translate(model9, glm::vec3(radius * -cos(angle), 0.0f, radius * sin(angle))); 
+        model9 = glm::scale(model9, glm::vec3(0.05f, 0.05f, 0.05f));  
         ourShader.setMat4("model", model9);
         dronModel.Draw(ourShader);
-
+               
         glm::mat4 model10 = glm::mat4(1.0f);
-        model10 = glm::translate(model10, glm::vec3(3.0f, 2.0f, -2.0f)); // Nueva posición
-        model10 = glm::scale(model10, glm::vec3(0.05f, 0.05f, 0.05f));  // Mismo escalado
-        model10 = glm::rotate(model10, angle, glm::vec3(sin(angle), -1.0f, cos(angle)));
-        model10 = glm::translate(model10, glm::vec3(radius * cos(angle), 2.0f, radius * sin(angle)));
+        model10 = glm::translate(model10, glm::vec3(3.0f, 2.0f, -2.0f)); 
+        model10 = glm::rotate(model10, angle, glm::vec3(-sin(angle), -1.0f, -cos(angle))); 
+        model10 = glm::translate(model10, glm::vec3(radius * -cos(angle), 0.0f, radius * -sin(angle))); 
+        model10 = glm::scale(model10, glm::vec3(0.05f, 0.05f, 0.05f));  
         ourShader.setMat4("model", model10);
         dronModel.Draw(ourShader);
 
         glm::mat4 model11 = glm::mat4(1.0f);
-        model11 = glm::translate(model11, glm::vec3(2.0f, 2.5f, 2.0f)); // Nueva posición
-        model11 = glm::scale(model11, glm::vec3(0.05f, 0.05f, 0.05f));  // Mismo escalado
-        model11 = glm::rotate(model11, angle, glm::vec3(sin(angle), -1.0f, cos(angle)));
-        model11 = glm::translate(model11, glm::vec3(radius * cos(angle), 2.0f, radius * sin(angle)));
+        model11 = glm::translate(model11, glm::vec3(2.0f, 2.5f, 2.0f)); 
+        model11 = glm::rotate(model11, angle, glm::vec3(sin(angle), -1.0f, -cos(angle))); 
+        model11 = glm::translate(model11, glm::vec3(radius * -cos(angle), 0.0f, radius * sin(angle)));
+        model11 = glm::scale(model11, glm::vec3(0.05f, 0.05f, 0.05f));  
         ourShader.setMat4("model", model11);
         dronModel.Draw(ourShader);
 
         glm::mat4 model12 = glm::mat4(1.0f);
-        model12 = glm::translate(model12, glm::vec3(0.0f, 3.0f, -3.0)); // Nueva posición
-        model12 = glm::scale(model12, glm::vec3(0.05f, 0.05f, 0.05f));  // Mismo escalado
-        model12 = glm::rotate(model12, angle, glm::vec3(sin(angle), -1.0f, cos(angle)));
-        model12 = glm::translate(model12, glm::vec3(radius * cos(angle), 2.0f, radius * sin(angle)));
+        model12 = glm::translate(model12, glm::vec3(0.0f, 3.0f, -3.0)); 
+        model12 = glm::rotate(model12, angle, glm::vec3(sin(angle), -1.0f, cos(angle))); 
+        model12 = glm::translate(model12, glm::vec3(radius * -cos(angle), 0.0f, radius * -sin(angle))); 
+        model12 = glm::scale(model12, glm::vec3(0.05f, 0.05f, 0.05f));  
         ourShader.setMat4("model", model12);
         dronModel.Draw(ourShader);
 
         glm::mat4 model13 = glm::mat4(1.0f);
-        model13 = glm::translate(model13, glm::vec3(-3.0f, 1.5f, 3.0)); // Nueva posición
-        model13 = glm::scale(model13, glm::vec3(0.05f, 0.05f, 0.05f));  // Mismo escalado
-        model13 = glm::rotate(model13, angle, glm::vec3(sin(angle), -1.0f, cos(angle)));
-        model13 = glm::translate(model13, glm::vec3(radius * cos(angle), 2.0f, radius * sin(angle)));
+        model13 = glm::translate(model13, glm::vec3(6.0f, 1.5f, 3.0));
+        model13 = glm::rotate(model13, angle, glm::vec3(sin(angle), -1.0f, -cos(angle)));
+        model13 = glm::translate(model13, glm::vec3(radius * -cos(angle), 0.0f, radius * sin(angle))); 
+        model13 = glm::scale(model13, glm::vec3(0.05f, 0.05f, 0.05f));  
         ourShader.setMat4("model", model13);
         dronModel.Draw(ourShader);
+
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
